9b108353732ad90a6570e1e0c81004801ea0a8e6
Tim Hutton <tihutt@microsoft.com>
2018-01-01 22:14:07 +0000

    Updated to SDL2. Changed to moving rectangle.

--- core.cpp
+++ core.cpp
@@ -1,13 +1,10 @@
-#include <stdio.h>
-#include <SDL/SDL.h>
-
-#ifdef __EMSCRIPTEN__
+#include <SDL2/SDL.h>
 #include <emscripten.h>
-#endif
+#include <cstdlib>
 
 struct context
 {
-    SDL_Surface *screen;
+    SDL_Renderer *renderer;
     int iteration;
 
     context() : iteration(0) {}
@@ -16,51 +13,41 @@ struct context
 void mainloop(void *arg)
 {
     context *ctx = static_cast<context*>(arg);
-    SDL_Surface *screen = ctx->screen;
+    SDL_Renderer *renderer = ctx->renderer;
+    
+    // example: draw a moving rectangle
+    
+    // red background
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    
+    // moving blue rectangle
+    SDL_Rect r;
+    r.x = ctx->iteration % 255;
+    r.y = 50;
+    r.w = 50;
+    r.h = 50;
+    SDL_SetRenderDrawColor( renderer, 0, 0, 255, 255 );
+    SDL_RenderFillRect( renderer, &r );
 
-    if (SDL_MUSTLOCK(screen)) SDL_LockSurface(screen);
-    for (int i = 0; i < 256; i++)
-    {
-        for (int j = 0; j < 256; j++)
-        {
-          #ifdef TEST_SDL_LOCK_OPTS
-            // Alpha behaves like in the browser, so write proper opaque pixels.
-            int alpha = 255;
-          #else
-            // To emulate native behavior with blitting to screen, alpha component is ignored. Test that it is so by outputting
-            // data (and testing that it does get discarded)
-            int alpha = (i+j) % 255;
-          #endif
-            int red = (j+ctx->iteration) % 255;
-            int green = 0;
-            int blue = 0;
-            *((Uint32*)screen->pixels + i * 256 + j) = SDL_MapRGBA(screen->format, red, green, blue, alpha);
-        }
-    }
-    if (SDL_MUSTLOCK(screen))
-    {
-        SDL_UnlockSurface(screen);
-    }
-    SDL_Flip(screen);
+    SDL_RenderPresent(renderer);
+
     ctx->iteration++;
 }
 
 int main()
 {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Surface *screen = SDL_SetVideoMode(256, 256, 32, SDL_SWSURFACE);
-
-    #ifdef TEST_SDL_LOCK_OPTS
-    EM_ASM("SDL.defaults.copyOnLock = false; SDL.defaults.discardOnLock = true; SDL.defaults.opaqueFrontBuffer = false;");
-    #endif
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_CreateWindowAndRenderer(255, 255, 0, &window, &renderer);
 
     context ctx;
-    ctx.screen = screen;
+    ctx.renderer = renderer;
 
     const int simulate_infinite_loop = 1; // call the function repeatedly
     const int fps = -1; // call the function as fast as can render (typically 60fps)
     emscripten_set_main_loop_arg(mainloop, &ctx, fps, simulate_infinite_loop);
 
-    SDL_Quit();
-    return 0;
+    return EXIT_SUCCESS;
 }
--- readme.md
+++ readme.md
@@ -4,4 +4,6 @@ This is the branch with the compiled code. See the source code on the [master br
 
 Build index.js and index.wasm with Emscripten:
 
-```emcc core.cpp -o index.js -s WASM=1```
+```emcc core.cpp -s WASM=1 -s USE_SDL=2 -O3 -o index.js```
+
+Then open index.html in a web browser.
